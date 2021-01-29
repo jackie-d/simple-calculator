d8c367c3bb3c538ee27ebbb4136bf7468cb293b1
Jackie Degl'Innocenti <info@jackied.it>
2021-01-29 17:15:11 +0100

    Created maven multi-modules project structure and backend spring boot app

---
+++ backend/src/main/java/com/demo/simplecalculator/backend/SimpleCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.simplecalculator.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCalculatorBackendApplication.class, args);
+	}
+
+}
