0a1d1ffdb5c751e5068b2c6f671026ebe19da6fd
Jackie Degl'Innocenti <info@jackied.it>
2021-01-30 16:21:35 +0100

    Create simple calculator class and test

---
+++ backend/src/main/java/com/demo/simplecalculator/backend/controllers/SimpleCalculator.java
@@ -0,0 +1,25 @@
+package com.demo.simplecalculator.backend.controllers;
+
+import com.demo.simplecalculator.backend.models.Equation;
+import com.demo.simplecalculator.backend.models.Equation.Sign;
+import org.springframework.stereotype.Component;
+
+public class SimpleCalculator {
+    
+    public double add(int number1, int number2) {
+        return Double.valueOf(number1) + Double.valueOf(number2);
+    }
+    
+    public double subtract(int number1, int number2) {
+        return Double.valueOf(number1) - Double.valueOf(number2);
+    }
+    
+    public double multiply(int number1, int number2) {
+        return Double.valueOf(number1) * Double.valueOf(number2);
+    }
+    
+    public double divide(int number1, int number2) {
+        return Double.valueOf(number1) / Double.valueOf(number2);
+    }
+    
+}
